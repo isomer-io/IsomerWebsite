0ccb5020826a2b6d69d0e67708e0fdf462bef452
Wesley Smith <wesleyfsmith@gmail.com>
2014-08-15 01:47:39 -0600

    Changed background photo, added new tag line.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,9 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+        //set the home page to allow fullscreen
+
+        $scope.allowFullScreen(true);
 	}
 ]);
