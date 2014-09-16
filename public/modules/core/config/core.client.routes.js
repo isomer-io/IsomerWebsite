d8cbd366ba43b5eae545c05ed27c8d0f09b3bfec
Wesley Smith <wesleyfsmith@gmail.com>
2014-09-15 19:05:33 -0600

    Worked more on the home page circles.

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,11 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
-		});
+		}).
+        state('tree', {
+                url: '/tree',
+                templateUrl: 'modules/core/views/tree.client.view.html'
+            })
+        ;
 	}
 ]);
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,9 +5,5 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-
-        //set the home page to allow fullscreen
-
-        $scope.allowFullScreen(true);
 	}
 ]);
