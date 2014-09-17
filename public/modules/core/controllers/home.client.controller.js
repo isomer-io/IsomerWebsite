7f1f0b6660b1d3acc41f02f0a7bb8b80c2b99fc4
Wesley Smith <wesleyfsmith@gmail.com>
2014-09-17 09:49:35 -0600

    More layout work on home page, fixed icons and text.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,27 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+        $scope.open = function() {
+            $scope.title ='Open';
+            $scope.subtitle = 'Our curriculum is free and open source. We use agile development on all projects.'
+        };
+
+        $scope.isomer = function() {
+            $scope.title = 'Isomer';
+            $scope.subtitle = 'The Open Source, blended classroom, non-profit, project-focused, community driven developer bootcamp.';
+        };
+
+        $scope.fullStack = function() {
+            $scope.title = 'Full-Stack';
+            $scope.subtitle = 'Understanding the entire stack is an essential engineering skill. From Databases to client, we\'ve got you covered.';
+        };
+
+        $scope.nonProfit = function() {
+            $scope.title = 'Non-Profit';
+            $scope.subtitle = 'Our aim is to provide the best education as accessibly as possible. Know that your tuition is only being allocated for as efficient of teaching as possible.';
+        };
+
+        $scope.isomer();
 	}
 ]);
