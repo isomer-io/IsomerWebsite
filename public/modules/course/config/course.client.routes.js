c1880451c2bf6222f639b67ef46fec0fd9fc7a71
Wesley Smith <wesleyfsmith@gmail.com>
2014-09-17 17:15:23 -0600

    More work on course page, added course details.

--- public/modules/course/config/course.client.routes.js
+++ public/modules/course/config/course.client.routes.js
@@ -8,6 +8,10 @@ angular.module('programs').config(['$stateProvider',
 		state('viewCourse', {
 			url: '/program',
 			templateUrl: 'modules/course/views/course.client.view.html'
-		});
+		}).
+        state('viewDeets', {
+                url: '/deets',
+                templateUrl: 'modules/course/views/deets.client.view.html'
+            });
 	}
 ]);
