88982b40a4ceb40c32afdba08026b6b6c8db73ef
Wesley Smith <wesleyfsmith@gmail.com>
2014-09-29 20:06:10 -0600

    A list of users can now be retrieved from the API.

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -4,6 +4,8 @@
  * Module dependencies.
  */
 var _ = require('lodash');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 /**
  * Extend user's controller
@@ -14,3 +16,41 @@ module.exports = _.extend(
 	require('./users/users.password'),
 	require('./users/users.profile')
 );
+
+/**
+ * Get the error message from error object
+ */
+var getErrorMessage = function(err) {
+    var message = '';
+
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'User already exists';
+                break;
+            default:
+                message = 'Something went wrong';
+        }
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) message = err.errors[errName].message;
+        }
+    }
+
+    return message;
+};
+
+module.exports.list = function(req, res) {
+
+
+    User.find().sort('-created').exec(function(err, submissions) {
+        if (err) {
+            return res.send(400, {
+                message: getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(submissions);
+        }
+    });
+};
