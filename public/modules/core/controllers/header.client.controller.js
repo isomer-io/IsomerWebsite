e1ccf7161b3f892b52918d1355c1c310e3ccfeed
kellsbells <kelleeballard@hotmail.com>
2014-10-28 19:02:24 -0600

    Adds ng-click and ng-show functionality

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -14,5 +14,40 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.$on('$stateChangeSuccess', function() {
 			$scope.isCollapsed = false;
 		});
+
+		$scope.myVar = false;
+		$scope.toggle = function() {
+			$scope.myVar = !$scope.myVar;
+		}
+
+		$scope.myVar1 = false;
+		$scope.toggle1 = function() {
+			$scope.myVar1 = !$scope.myVar1;
+		}
+
+		$scope.myVar2 = false;
+		$scope.toggle2 = function() {
+			$scope.myVar2 = !$scope.myVar2;
+		}
+
+		$scope.myVar3 = false;
+		$scope.toggle3 = function() {
+			$scope.myVar3 = !$scope.myVar3;
+		}
+
+		$scope.myVar4 = false;
+		$scope.toggle4 = function() {
+			$scope.myVar4 = !$scope.myVar4;
+		}
+
+		$scope.myVar5 = false;
+		$scope.toggle5 = function() {
+			$scope.myVar5 = !$scope.myVar5;
+		}
+
+		$scope.myVar6 = false;
+		$scope.toggle6 = function() {
+			$scope.myVar6 = !$scope.myVar6;
+		}
 	}
 ]);
