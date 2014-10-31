15fd4366f4661cfac73e83a27c284389b4dbc3c4
Wesley Smith <wesleyfsmith@gmail.com>
2014-10-30 21:44:25 -0600

    Applied new layout.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -6,26 +6,59 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
-        $scope.open = function() {
-            $scope.title ='Open';
-            $scope.subtitle = 'Our curriculum is free and open source. We use agile development on all projects.';
+        //$scope.open = function() {
+        //    $scope.title ='Open';
+        //    $scope.subtitle = 'Our curriculum is free and open source. We use agile development on all projects.';
+        //};
+        //
+        //$scope.isomer = function() {
+        //    $scope.title = 'Isomer';
+        //    $scope.subtitle = 'The Open Source, blended classroom, non-profit, project-focused, community driven developer bootcamp.';
+        //};
+        //
+        //$scope.fullStack = function() {
+        //    $scope.title = 'Full-Stack';
+        //    $scope.subtitle = 'Understanding the entire stack is an essential engineering skill. From Databases to client, we\'ve got you covered.';
+        //};
+        //
+        //$scope.nonProfit = function() {
+        //    $scope.title = 'Non-Profit';
+        //    $scope.subtitle = 'Our aim is to provide the best education as accessibly as possible. Know that your tuition is only being allocated for as efficient of teaching as possible.';
+        //};
+        //
+        //$scope.isomer();
+
+        $scope.selection = {
+            aboutIsomer: true,
+            community: false,
+            curriculum: false,
+            realWorldApplication: false,
+            students: false
         };
 
-        $scope.isomer = function() {
-            $scope.title = 'Isomer';
-            $scope.subtitle = 'The Open Source, blended classroom, non-profit, project-focused, community driven developer bootcamp.';
+        $scope.selectContent = function(content) {
+            if (content === 'About Isomer') {
+                $scope.selection
+            }
+            if (content === 'Community') {
+
+            }
+            if (content === 'Curriculum') {
+
+            }
+            if (content === 'Real World Application') {
+
+            }
+            if (content === 'Students') {
+
+            }
         };
 
-        $scope.fullStack = function() {
-            $scope.title = 'Full-Stack';
-            $scope.subtitle = 'Understanding the entire stack is an essential engineering skill. From Databases to client, we\'ve got you covered.';
-        };
+        $scope.detailView = '/modules/core/views/community.client.view.html';
 
-        $scope.nonProfit = function() {
-            $scope.title = 'Non-Profit';
-            $scope.subtitle = 'Our aim is to provide the best education as accessibly as possible. Know that your tuition is only being allocated for as efficient of teaching as possible.';
-        };
+        $scope.getView = function() {
+            return '/modules/core/views/community.client.view.html';
+        }
 
-        $scope.isomer();
 	}
 ]);
