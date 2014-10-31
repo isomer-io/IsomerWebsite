ec53b64a7203b9ad1028fa5e88dfb18791a6d087
Chris <christanseer@hotmail.com>
2014-10-31 15:44:17 -0600

    added routes to sub-pages,  and added an instructor page to be completed later

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -12,13 +12,53 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		}).
-        state('tree', {
-                url: '/tree',
-                templateUrl: 'modules/core/views/asynchronous.client.view.html'
-            }).
+
+		//what's this route for?
+		//
+        //state('tree', {
+        //       url: '/tree',
+	 	//       templateUrl: 'modules/core/views/asynchronous.client.view.html'
+		//}).
+
         state('asynchronous', {
-                url: '/asynchronous',
-                templateUrl: 'modules/core/views/community.client.view.html'
-            });
+			url: '/asynchronous',
+			templateUrl: 'modules/core/views/community.client.view.html'
+		}).
+
+		state('community', {
+			url: '/community',
+			templateUrl: 'modules/core/views/community.client.view.html'
+		}).
+
+		state('fullstack', {
+			url: '/fullstack',
+			templateUrl: 'modules/core/views/fullstack.client.view.html'
+		}).
+
+		state('instructors', {
+		url: '/instructors',
+		templateUrl: 'modules/core/views/instructors.client.view.html'
+		}).
+
+		state('pricing', {
+			url: '/pricing',
+			templateUrl: 'modules/core/views/pricing.client.view.html'
+		}).
+
+		state('realWorld', {
+			url: '/realWorld',
+			templateUrl: 'modules/core/views/real-world.client.view.html'
+		}).
+
+		state('students', {
+			url: '/students',
+			templateUrl: 'modules/core/views/students.client.view.html'
+		}).
+
+		state('welcome', {
+			url: '/welcome',
+			templateUrl: 'modules/core/views/welcome.client.view.html'
+		});
+
 	}
 ]);
