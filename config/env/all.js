ddb1f725220e4344b02a3f89bb7160044b5f8a44
Wesley Smith <wesleyfsmith@gmail.com>
2014-12-07 21:20:49 -0700

    Added instructors to student view page.

--- config/env/all.js
+++ config/env/all.js
@@ -16,7 +16,7 @@ module.exports = {
 //				'public/lib/bootstrap/dist/css/bootstrap.css',
 //				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
                 'public/lib/bootswatch/paper/bootstrap.css',
-                'public/lib/font-awesome/css/font-awesome.css'
+                'public/lib/fontawesome/css/font-awesome.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
